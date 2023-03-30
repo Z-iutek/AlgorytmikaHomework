10adeb84f04cb84f088546f3fda3d859183730bf
TomPaCKam <wrx81713@student.wsb.wroclaw.pl>
2023-03-30 20:51:32 +0200

    First time measures for clean up algorithms

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from sortingFuncions import insertion_sort, merge_sort
 from searchFunctions import binary_search, linear_search
 
 # 1.Wylosować n- elementów z  zakresu od 0 – 100 (Random.randint(0,100))
+
 n = int(input("Podaj wartość n: "))
 list_of_n_elements = []
 
 for i in range(n):
     list_of_n_elements.append(randint(0, 100))
+
 # 2.Wylosować wartość z przedziału <0,100> jako tzw. wartość śmieciową
+
 garbage_value = randint(0, 100)
 
 # 3.Wyczyścić dane za pomocą algorytmów data clean-ups:
@@ -18,6 +21,7 @@ garbage_value = randint(0, 100)
 # B)shuffle-left albo converging pointers
 
 # A)copy-over:
+
 print("\nCzyszczenie przy pomocy algorytmu copy-over:\n")
 temp1 = list_of_n_elements[:]
 print("Wartość śmieciowa: " + str(garbage_value))
@@ -27,6 +31,7 @@ print("Lista po sprzątaniu:    " + str(cleaned_up_list_2))
 print("\n")
 
 # B) converging pointers
+
 print("Czyszczenie przy pomocy algorytmu converging pointers:\n")
 temp2 = list_of_n_elements[:]
 print("Wartość śmieciowa: " + str(garbage_value))
@@ -37,8 +42,8 @@ print("\n")
 
 
 # 4.Posortować listę za pomocą:
-# A)insertion sort
-# B)merge sort
+# A) insertion sort
+# B) merge sort
 
 # A)insertion sort:
 print("Sortowanie przy pomocy algorytmu insertion sort:\n")
@@ -56,9 +61,9 @@ sorted_list2 = merge_sort(temp4)
 print("Lista po sortowaniu:    " + str(sorted_list2))
 print("\n")
 
-# 5.Wyszukać na liście wylosowaną wartość z zakresu <0,100> bez uwzględnienia wartości śmieciowej
-# A)binary search
-# B)linear/sequential search - (nie trzeba wcześniej sortować)
+# 5. Wyszukać na liście wylosowaną wartość z zakresu <0,100> bez uwzględnienia wartości śmieciowej
+# A) binary search
+# B) linear/sequential search - (nie trzeba wcześniej sortować)
 
 # Losujemy wartość <0,100> z wykluczeniem wartości śmieciowej
 flag = True
@@ -76,3 +81,84 @@ print("Wartość: " + str(value_to_find) + " znajduje się pod indeksem: " + str
 # B) linear search
 temp6 = list_of_n_elements[:]
 print("Wartość: " + str(value_to_find) + " znajduje się pod indeksem: " + str(linear_search(temp6, value_to_find)))
+
+print("\n***************************************************************************\n")
+
+# 6.Porównać czas na liście n=100, n=10000, n= 500000 ->
+# A) czyszczenie (2 alg.)
+# B) sortowanie (2 algorytmy + binary search)
+# C) wyszukiwanie za pomocą sequential search
+
+
+# Stworzę sobię zestaw list n-elementowych oraz nową wartość śmieciową (GV2)
+n_100 = []
+for i in range(100):
+    n_100.append(randint(0, 100))
+n_10000 = []
+for i in range(10000):
+    n_10000.append(randint(0, 100))
+n_500000 = []
+for i in range(50000):
+    n_500000.append(randint(0, 100))
+
+garbage_value_2 = randint(0, 100)
+
+# A) czyszczenie:
+
+temp1 = n_100[:]
+temp2 = n_100[:]
+
+time_CP_S = time()
+converging_pointers(garbage_value_2, temp1)
+time_CP_F = time()
+end_time_CP = time_CP_F - time_CP_S
+print("Dla n = 100, czyszczenie przez Converging Pointers wynosi: %.15f" % end_time_CP)
+time_CO_S = time()
+copy_over(garbage_value_2, temp2)
+time_CO_F = time()
+end_time_CO = time_CO_F - time_CO_S
+print("Dla n = 100, czyszczenie przez Copy Over wynosi: %.15f" % end_time_CO)
+
+
+
+temp1 = n_10000[:]
+temp2 = n_10000[:]
+
+time_CP_S = time()
+converging_pointers(garbage_value_2, temp1)
+time_CP_F = time()
+end_time_CP = time_CP_F - time_CP_S
+print("Dla n = 10000, czyszczenie przez Converging Pointers wynosi: %.15f" % end_time_CP)
+time_CO_S = time()
+copy_over(garbage_value_2, temp2)
+time_CO_F = time()
+end_time_CO = time_CO_F - time_CO_S
+print("Dla n = 10000, czyszczenie przez Copy Over wynosi: %.15f" % end_time_CO)
+
+
+temp1 = n_500000[:]
+temp2 = n_500000[:]
+
+time_CP_S = time()
+converging_pointers(garbage_value_2, temp1)
+time_CP_F = time()
+end_time_CP = time_CP_F - time_CP_S
+print("Dla n = 500000, czyszczenie przez Converging Pointers wynosi: %.15f" % end_time_CP)
+time_CO_S = time()
+copy_over(garbage_value_2, temp2)
+time_CO_F = time()
+end_time_CO = time_CO_F - time_CO_S
+print("Dla n = 500000, czyszczenie przez Copy Over wynosi: %.15f" % end_time_CO)
+
+print("\n***************************************************************************\n")
+
+# B) sortowanie + binary search
+
+
+
+
+
+
+
+
+
